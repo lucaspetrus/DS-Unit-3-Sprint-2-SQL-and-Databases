42f50a5c097dab83d6f6429d1aa34c578fb467da
lucaspetrus <lpetrus16@gmail.com>
2020-09-09 14:17:13 -0500

    modified for class

--- module3-nosql-and-document-oriented-databases/Assignment 3.py
+++ module3-nosql-and-document-oriented-databases/Assignment 3.py
@@ -1,5 +1,9 @@
 import pymongo
 
+# I think mongo is a bit more complex than sqlite and postgres
+# But the benefit is you can do more with mongo and share easier
+
+
 client = pymongo.MongoClient("mongodb+srv://lpetrus16:WfCtc5j15fDWwmyw@cluster0.oxoh9.gcp.mongodb.net/"
                              + "test?retryWrites=true&w=majority")
 db = client.test
@@ -12,7 +16,10 @@ rpg_doc = {
     'sql_key': rpg_character[0],
     'name': rpg_character[1],
     'hp': rpg_character[2],
-    'level': rpg_character[3]
+    'level': rpg_character[3],
+    'health': rpg_character[4],
+    'skill': rpg_character[5],
+    'dexterity': rpg_character[6]
 }
 db.test.insert_one(rpg_doc)
 
